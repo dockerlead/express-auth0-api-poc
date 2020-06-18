133d1116b84229021f8a90f2c01e051824af8182
dockerlead <dockerlead@mail.com>
2020-06-18 12:41:01 -0500

    :tada:Init

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const jwt = require("express-jwt");
+const jwksRsa = require("jwks-rsa");
+
+// Create a new Express app
+const app = express();
+
+// Set up Auth0 configuration
+const authConfig = require("./auth_config.json")
+
+// Define middleware that validates incoming bearer tokens
+// using JWKS from crazyoptimist.us.auth0.com
+const checkJwt = jwt({
+  secret: jwksRsa.expressJwtSecret({
+    cache: true,
+    rateLimit: true,
+    jwksRequestsPerMinute: 5,
+    jwksUri: `https://${authConfig.domain}/.well-known/jwks.json`
+  }),
+
+  audience: authConfig.audience,
+  issuer: `https://${authConfig.domain}/`,
+  algorithm: ["RS256"]
+});
+
+// Define an endpoint that must be called with an access token
+app.get("/", checkJwt, (req, res) => {
+  res.send({
+    msg: "Your Access Token was successfully validated!"
+  });
+});
+
+// Start the app
+app.listen(3000, () => console.log('API listening on 3000'));
